99baef18d8598488646ce99bbac96a79777e7fa5
Carlos Cabrera <cadacaes@hotmail.com>
2022-10-07 16:08:32 -0300

    FT-2 add rendering product item tests

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -3,39 +3,45 @@ const ProductItem = ({ name, description, price = 0, image }) => {
     return (
         <>
             <figure className="image is-1by1">
-                <img style={{objectFit: "cover"}} src={image} alt={name} />
+                {/*TODO: add default image when the image doesn't exist*/}
+                <img style={{ objectFit: "cover" }} src={image || ''} alt={name}
+                    onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping
+                        currentTarget.src = "https://as1.ftcdn.net/v2/jpg/02/11/01/88/500_F_211018896_81wCZzIA4pu0kYnu1zOmYw68hMsm1CEz.jpg";
+                    }}
+                />
             </figure>
-            <div class="columns is-mobile mt-3">
-                <div class="column is-two-thirds">
-                    <p className="title is-5">{name}</p>
+            <div className="columns is-mobile mt-3">
+                <div className="column is-two-thirds">
+                    <h5 className="title is-5">{name}</h5>
                 </div>
-                <div class="column has-text-right">
-                    <p className="title is-4 has-text-primary-dark">{`${price}$`}</p>
+                <div className="column has-text-right">
+                    <h4 className="title is-4 has-text-primary-dark">{`${price}$`}</h4>
                 </div>
             </div>
 
             <p className="subtitle is-6 mt-3">{description}</p>
 
-            <div class="columns is-mobile">
-                <div class="column is-two-thirds">
-                    <div class="buttons">
-                        <button class="button is-primary is-light mr-0 mb-0" aria-label="Minus">
-                            <span class="icon is-small">
-                                <i class="fa-solid fa-minus"></i>
+            <div className="columns is-mobile">
+                <div className="column is-two-thirds">
+                    <div className="buttons">
+                        <button className="button is-primary is-light mr-0 mb-0" aria-label="Minus">
+                            <span className="icon is-small">
+                                <i className="fa-solid fa-minus"></i>
                             </span>
                         </button>
                         <span className="mr-1 ml-1">0</span>
-                        <button class="button is-primary is-light mb-0" aria-label="Plus">
-                            <span class="icon is-small">
-                                <i class="fa-solid fa-plus"></i>
+                        <button className="button is-primary is-light mb-0" aria-label="Plus">
+                            <span className="icon is-small">
+                                <i className="fa-solid fa-plus"></i>
                             </span>
                         </button>
                     </div>
                 </div>
-                <div class="column has-text-right">
-                    <button class="button is-primary" aria-label="Add to Cart" title="Add to Cart">
-                        <span class="icon is-small">
-                            <i class="fa-solid fa-cart-plus"></i>
+                <div className="column has-text-right">
+                    <button className="button is-primary" aria-label="Add to Cart" title="Add to Cart">
+                        <span className="icon is-small">
+                            <i className="fa-solid fa-cart-plus"></i>
                         </span>
                     </button>
                 </div>
