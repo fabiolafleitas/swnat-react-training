ed6dd72520cbef3dda3e4461d43301217cb314ad
Fabiola <fabiola@softwarenatura.com>
2022-10-14 10:50:12 -0300

    FT-3 Add tests to Image and ProductItem components

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,28 +1,27 @@
-import Card from "../ui/Card";
-import CardGrid from "../ui/CardGrid";
-import ProductItem from "./ProductItem";
-import serverDown from "../../assets/images/server-down.svg";
+import CardGrid from '../ui/CardGrid'
+import ProductItem from './ProductItem'
+import serverDown from '../../assets/images/server-down.svg'
 
-const ProductList = ({products = []}) => {
-    return (
-        <div className="section">
-            <h4 className="title is-4">Products</h4>
-            {products.length ?
-                <CardGrid>
-                    {products.map(product => {
-                        return (
-                            <ProductItem {...product} key={product.id}/>
-                        )
-                    })}
-                </CardGrid>
-                :
-                <div className="container is-centered has-text-centered	">
-                    <img src={serverDown} alt="Server Down" style={{width: "300px"}}/>
-                    <h5 className="title is-5 has-text-primary-dark">No products found</h5>
-                </div>
-            }
+const ProductList = ({ products = [] }) => {
+  return (
+    <div className="section">
+      <h4 className="title is-4">Products</h4>
+      {products.length ? (
+        <CardGrid>
+          {products.map((product) => {
+            return <ProductItem {...product} key={product.id} />
+          })}
+        </CardGrid>
+      ) : (
+        <div className="container is-centered has-text-centered	">
+          <img src={serverDown} alt="Server Down" style={{ width: '300px' }} />
+          <h5 className="title is-5 has-text-primary-dark">
+            No products found
+          </h5>
         </div>
-    )
+      )}
+    </div>
+  )
 }
 
-export default ProductList;
+export default ProductList
--- src/components/ui/Image.js
+++ src/components/ui/Image.js
@@ -1,44 +1,40 @@
 import { useState } from 'react'
 import taken from '../../assets/images/taken.svg'
 
-const getFallBackImageSrc = (selection) => {
-  switch(selection) {
-    case 'taken':
-      return taken
-    default:
-      return taken
+const Image = ({
+  imgSrc,
+  altText
+}) => {
+  const [loading, setLoading] = useState(true)
+  const [failed, setFailed] = useState(false)
+
+  const useFallback = failed || !imgSrc
+
+  const source = useFallback ? taken : imgSrc
+  const description = useFallback ? 'Donut monster taken' : altText
+
+  const styles = {
+    visibility: loading ? 'hidden' : 'visible',
+    objectFit: useFallback ? 'contain' : 'cover',
   }
-}
 
-const Image = ({ 
-  imgSrc, 
-  altText, 
-  fallbackImg = 'taken', 
-  altTextFallback = 'Donut monster taken' }) => {
-    const [loading, setLoading] = useState(true)
-    const [failed, setFailed] = useState(false)
+  const onImageLoadError = () => {
+    setFailed(true)
+  }
 
-    const useFallback = failed || !imgSrc
+  const onLoadImage = () => {
+    setLoading(false)
+  }
 
-    const source = useFallback ? getFallBackImageSrc(fallbackImg) : imgSrc
-    const description = useFallback ? altTextFallback : altText
-     
-    const styles = {
-      visibility: loading ? 'hidden' : 'visible',
-      objectFit: useFallback ? 'contain' : 'cover'
-    }
-
-    const onImageLoadError = () => {
-      setFailed(true)
-    }
-
-    const onLoadImage = () => {
-      setLoading(false)
-    }
-
-    return (
-      <img style={styles} src={source} alt={description} onError={onImageLoadError} onLoad={onLoadImage}/>
-    )
+  return (
+    <img
+      style={styles}
+      src={source}
+      alt={description}
+      onError={onImageLoadError}
+      onLoad={onLoadImage}
+    />
+  )
 }
 
 export default Image
