74dc477a50a42ec757a56f1b458050b95e9470b0
Carlos Cabrera <carlos.cabrera@mentormate.com>
2022-10-27 14:13:42 -0300

    FT-4 fix counter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import Header from './components/ui/Header';
-import Hero from './components/ui/Hero';
-import ProductList from './components/product/ProductList';
-import {products} from "./components/product/mockData";
-import CartContextProvider from "./components/cart/CartContext";
+import './App.css'
+import Header from './components/ui/Header'
+import Hero from './components/ui/Hero'
+import ProductList from './components/product/ProductList'
+import { products } from "./components/product/mockData"
+import { CartContextProvider } from "./components/cart/CartContext"
 
 function App() {
     return (
         <CartContextProvider>
-            <Header/>
+            <Header />
             <div className="container is-fullhd">
-                <Hero/>
-                <ProductList products={products}/>
+                <Hero />
+                <ProductList products={products} />
             </div>
         </CartContextProvider>
-    );
+    )
 }
 
-export default App;
+export default App
