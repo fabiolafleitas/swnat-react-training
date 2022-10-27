3a77a999e059bd526900d8fa429d5c0815e0a2c9
Carlos Cabrera <carlos.cabrera@mentormate.com>
2022-10-27 15:32:16 -0300

    FT-4 WIP cart context and reducer

--- src/components/product/Counter.js
+++ src/components/product/Counter.js
@@ -1,48 +1,46 @@
-import {useReducer} from "react";
+import { useImperativeHandle, useReducer } from "react"
 
 function init(initialCount) {
-    return {count: initialCount};
+    return { count: initialCount }
 }
 
 function reducer(state, action) {
     switch (action.type) {
         case 'increment':
-            return {count: state.count + 1};
+            return { count: state.count + 1 }
         case 'decrement':
-            return {count: state.count - 1};
+            return { count: state.count - 1 }
         case 'reset':
-            return init(action.payload);
+            return init(action.payload)
         default:
-            throw new Error();
+            throw new Error()
     }
 }
 
 
-const Counter = ({initialCount}) => {
+const Counter = ({ initialCount }, _ref) => {
 
-    const [state, dispatch] = useReducer(reducer, initialCount, init);
-
-
+    const [state, dispatch] = useReducer(reducer, initialCount, init)
 
     return (
         <div className="buttons">
             <button
-                disabled={!state.count} onClick={() => dispatch({type: 'decrement'})}
+                disabled={!state.count} onClick={() => dispatch({ type: 'decrement' })}
                 className={"button is-small mr-0 mb-0"} aria-label="Minus">
-                            <span className="icon is-small">
-                                <i className="fa-solid fa-minus"/>
-                            </span>
+                <span className="icon is-small">
+                    <i className="fa-solid fa-minus" />
+                </span>
             </button>
             <span className="mr-1 ml-1">{state.count}</span>
             <button
-                onClick={() => dispatch({type: 'increment'})}
+                onClick={() => dispatch({ type: 'increment' })}
                 className="button is-small mb-0" aria-label="Plus">
-                            <span className="icon is-small">
-                                <i className="fa-solid fa-plus"/>
-                            </span>
+                <span className="icon is-small">
+                    <i className="fa-solid fa-plus" />
+                </span>
             </button>
         </div>
     )
 }
 
-export default Counter;
+export default Counter
